508055a0496dc568db5d28faaa7efb169763b389
Muhammad Amit Hasan <bhamithasan@gmail.com>
2022-05-12 11:45:36 +0600

    Modal time slot collection done

--- src/Pages/Appointment/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments.js
@@ -24,7 +24,7 @@ const AvailableAppointments = ({ date }) => {
                     ></Service>)
                 }
             </div>
-            {treatment && <BookingModal treatment={treatment}></BookingModal>}
+            {treatment && <BookingModal treatment={treatment} setTreatment={setTreatment} date={date}></BookingModal>}
         </div>
     );
 };
--- src/Pages/Appointment/BookingModal.js
+++ src/Pages/Appointment/BookingModal.js
@@ -1,18 +1,34 @@
+import { format } from 'date-fns';
 import React from 'react';
 
-const BookingModal = ({ treatment }) => {
+const BookingModal = ({ treatment, date, setTreatment }) => {
     const { name, slots } = treatment;
+    const handleBooking = e => {
+        e.preventDefault();
+        const slot = e.target.slot.value;
+        console.log(slot)
+        setTreatment(null)
+    }
     return (
         <div>
             <input type="checkbox" id="Booking-modal" class="modal-toggle" />
             <div class="modal modal-bottom sm:modal-middle">
                 <div class="modal-box">
                     <label for="Booking-modal" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
-                    <h3 class="font-bold text-lg">{name}</h3>
-                    <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
-                    <div class="modal-action">
-                        <label for="Booking-modal" class="btn">Yay!</label>
-                    </div>
+                    <h3 class="font-bold text-lg text-center">{name}</h3>
+
+                    <form onSubmit={handleBooking} className='grid grid-cols-1 gap-2 mt-5 justify-items-center'>
+                        <input type="text" value={format(date, 'PP')} disabled class="input input-bordered input-secondary w-full max-w-xs" />
+                        <select name='slot' class="select select-primary w-full max-w-xs">
+                            {
+                                slots.map(slot => <option key={slot._id} value={slot}>{slot}</option>)
+                            }
+                        </select>
+                        <input type="text" placeholder="Name" class="input input-bordered input-secondary w-full max-w-xs" />
+                        <input type="text" placeholder="Email" class="input input-bordered input-secondary w-full max-w-xs" />
+                        <input type="text" placeholder="Phone Number" class="input input-bordered input-secondary w-full max-w-xs" />
+                        <input type="submit" class="btn btn-secondary w-full max-w-xs" />
+                    </form>
                 </div>
             </div>
         </div >
